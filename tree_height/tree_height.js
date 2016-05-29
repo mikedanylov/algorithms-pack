84ee811fd32c233c1bdc593bc2be3d4672190a86
Mykhailo Danylov <mike.danylov@gmail.com>
2016-05-29 23:28:10 +0300

    Pass all tests

--- tree_height/tree_height.js
+++ tree_height/tree_height.js
@@ -1,51 +1,44 @@
-function findPositionsInArray(k, arr) {
-    var positions = [];
+function getTreeHeight(str) {
+    var arr = str.split(' ').map(function(item) {
+        return parseInt(item, 10);
+    });
+    var heights = [];
+    var currDepth = 1;
+    var maxDepth = 0;
+
+    // loop over all nodes in the tree
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == k) {
-            positions.push(i);
+        if (arr[i] === -1) {
+            heights.push(1);
+            continue;
         }
-    }
-    return positions;
-}
 
-function treeHeight(string) {
-    var arr = string.split(' ');
-    var depth = 1;
-    var k = -1;
-
-    // until any element is not negative
-    while(Math.max.apply(null, arr) > -1) {
-        var b = findPositionsInArray(k, arr);
-        // console.log('index of root: ', b);
-
-        if (b.length) {
-            k--;
-            depth++;
-            for (var i = 0; i < arr.length; i++) {
-                var tmp = parseInt(arr[i]);
-                if (b.indexOf(tmp) !== -1) {
-                    arr[i] = k;
+         currDepth = 1;
+        // find distance to root for each node
+        tmp = i; // node index
+        while (tmp !== -1) { // while not a root node
+            tmp = arr[tmp];
+            if (heights[tmp]) {
+                currDepth += heights[tmp];
+                if (currDepth > maxDepth) {
+                    maxDepth = currDepth;
                 }
+                break;
+            }
+            if (tmp !== -1) {
+                currDepth++;
+            }
+            if (currDepth > maxDepth) {
+                maxDepth = currDepth;
             }
         }
+
+        // save depth for current element
+        heights.push(currDepth);
     }
 
-    return depth;
+    return maxDepth;
 }
-
-// process.stdin.setEncoding('utf8');
-
-// var line = 1;
-
-// process.stdin.on('readable', function () {
-//     var chunk = process.stdin.read();
-//     if (chunk !== null && line !== 1) {
-//         var result = treeHeight(chunk);
-//         process.stdout.write(String(result) + "\n");
-//         process.exit();
-//     }
-//     line++;
-// });
 
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -57,7 +50,7 @@ var lineNum = 1;
 
 rl.on('line', function(line){
     if (lineNum !== 1) {
-        console.log(treeHeight(line));
+        console.log(getTreeHeight(line));
         process.exit();
     }
     lineNum++;
