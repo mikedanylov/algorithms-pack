b7d93638528b2f037ad0d0605ed2f3c9e3326d1f
Mykhailo Danylov <mike.danylov@gmail.com>
2016-06-06 00:19:41 +0300

    Initial implementation

---
+++ make_heap/build_heap.js
@@ -0,0 +1,59 @@
+function Heap(array) {
+    if (array && array.length) {
+        this.array = array;
+        this.swapCount = 0;
+        this.buildHeap(array);
+    } else {
+        throw new Error('Heap: should have Array parameter');
+    }
+}
+
+Heap.prototype.buildHeap = function (array) {
+    this.size = array.length;
+
+    for (var i = parseInt(array.length / 2); i >= 0; i--) {
+        this.siftDown(i + 1);
+    }
+};
+
+Heap.prototype.siftDown = function (index) {
+    var maxIndex = index;
+    var leftChild = this.leftChild(index);
+    var rightChild = this.rightChild(index);
+    var arr = this.array;
+    var len = this.array.length;
+
+    if (leftChild <= len && arr[leftChild - 1] < arr[maxIndex - 1]) {
+        maxIndex = leftChild;
+    }
+
+    if (rightChild <= len && arr[rightChild - 1] < arr[maxIndex - 1]) {
+        maxIndex = rightChild;
+    }
+
+    if (index !== maxIndex) {
+        var tmp = arr[index - 1];
+
+        this.swapCount += 1;
+        arr[index - 1] = arr[maxIndex - 1];
+        arr[maxIndex - 1] = tmp;
+        this.siftDown(maxIndex);
+    }
+};
+
+Heap.prototype.parent = function (index) {
+    return parseInt(index / 2);
+};
+
+Heap.prototype.leftChild = function (index) {
+    return 2 * index;
+};
+
+Heap.prototype.rightChild = function (index) {
+    return 2 * index + 1;
+};
+
+var heap = new Heap([5, 4, 3, 2, 1]);
+console.log(heap);
+var heap2 = new Heap([1, 2, 3, 4, 5]);
+console.log(heap2);
