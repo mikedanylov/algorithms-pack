478494bb1f1960bcfe78320c635943fc4939f5fb
Mykhailo Danylov <mike.danylov@gmail.com>
2016-06-19 01:20:03 +0300

    Refactor JobQueue

--- job_queue/job_queue.js
+++ job_queue/job_queue.js
@@ -4,14 +4,13 @@ function JobQueue(nThreads, jobsArr) {
         quantity: nThreads,
         free: [],
         busy: []
-    },
+    };
     this.tick = 0;
     this.logs = [];
 
     for (var i = 0; i < nThreads; i++) {
         this.threads.free.push({
-            number: i,
-            index: i
+            number: i
         });
     }
 }
@@ -20,11 +19,8 @@ JobQueue.prototype.assignJob = function (jobIndex) {
     var thread = this._popThread('free');
 
     if (thread) {
-        thread.currentJob = {
-            index: jobIndex,
-            startingTick: this.tick,
-            endingTick: this.tick + this.jobs[jobIndex]
-        }
+        thread.startingTick = this.tick;
+        thread.endingTick = this.tick + this.jobs[jobIndex];
         this._pushThread('busy', thread);
     }
 };
@@ -42,7 +38,7 @@ JobQueue.prototype._pushThread = function (type, thread) {
         });
         this._siftUpBusy(this.threads.busy.length - 1);
     } else if (type === 'free') {
-        thread.index = this.threads.free.length;
+        // thread.index = this.threads.free.length;
         this.threads.free.push(thread);
     }
 };
@@ -84,100 +80,119 @@ JobQueue.prototype._rightChild = function (i) {
 };
 
 JobQueue.prototype._siftUpBusy = function (index) {
-    var nodeEndingTick = this.threads.busy[index].currentJob.endingTick;
-    var parentIndex;
-    var parentEndingTick;
+    var queue = this.threads.busy;
+    var node = queue[index];
+    var parentIndex = this._parent(index);
+    var parentNode = queue[parentIndex];
 
-    while (index > 0 && nodeEndingTick < parentEndingTick) {
-        parentIndex = this._parent(index);
-        parentEndingTick = this.threads.busy[parentIndex].currentJob.endingTick;
+    while (parentNode &&
+        (node.endingTick < parentNode.endingTick ||
+        (node.endingTick === parentNode.endingTick && node.number < parentNode.number))) {
 
-        var tmp = this.threads.busy[parentIndex];
-        this.threads.busy[parentIndex] = this.threads.busy[index];
-        this.threads.busy[index] = tmp;
-
+        var tmp = queue[parentIndex];
+        queue[parentIndex] = queue[index];
+        queue[index] = tmp;
         index = parentIndex;
+
+        parentIndex = this._parent(index);
+        parentNode = queue[parentIndex];
     }
 };
 
 JobQueue.prototype._siftDownFree = function (index) {
-    var maxIndex = index;
+    var higherPrioIndex = index;
     var leftChildIndex = this._leftChild(index);
     var rightChildIndex = this._rightChild(index);
     var size = this.threads.free.length;
     var queue = this.threads.free;
 
-    if (leftChildIndex < size && queue[leftChildIndex].number < queue[maxIndex].number) {
-        maxIndex = leftChildIndex;
-    }
-    if (rightChildIndex < size && queue[rightChildIndex].number < queue[maxIndex].number) {
-        maxIndex = rightChildIndex;
+    if (leftChildIndex < size &&
+        queue[leftChildIndex].number < queue[index].number) {
+        higherPrioIndex = leftChildIndex;
     }
 
-    if (index !== maxIndex) {
+    if (rightChildIndex < size &&
+        queue[rightChildIndex].number < queue[index].number &&
+        queue[rightChildIndex].number < queue[leftChildIndex].number) {
+        higherPrioIndex = rightChildIndex;
+    }
+
+    if (index !== higherPrioIndex) {
         var tmp = queue[index];
-        queue[index] = queue[maxIndex];
-        queue[maxIndex] = tmp;
+        queue[index] = queue[higherPrioIndex];
+        queue[higherPrioIndex] = tmp;
 
-        this._siftDownFree(maxIndex);
+        this._siftDownFree(higherPrioIndex);
     }
 };
 
 JobQueue.prototype._siftDownBusy = function (index) {
-    var maxIndex = index;
+    // index is used only to get child elements
+    // the comparison happens against jobEndTime and threadPriotiry
+
+    var nextIndex = index;
+    var size = this.threads.busy.length;
     var leftChildIndex = this._leftChild(index);
     var rightChildIndex = this._rightChild(index);
-    var size = this.threads.busy.length;
-    var queue = this.threads.busy;
-    var currEndingTick = queue[maxIndex] ? queue[maxIndex].currentJob.endingTick : null;
-    var leftEndingTick = queue[leftChildIndex] ? queue[leftChildIndex].currentJob.endingTick : null;
-    var rightEndingTick = queue[rightChildIndex] ? queue[rightChildIndex].currentJob.endingTick : null;
+    var currElement = this.threads.busy[index];
+    var leftChild = this.threads.busy[leftChildIndex];
+    var rightChild = this.threads.busy[rightChildIndex];
 
-    if (leftChildIndex < size && leftEndingTick < currEndingTick) {
-        maxIndex = leftChildIndex;
-    }
-    if (rightChildIndex < size && rightEndingTick < currEndingTick) {
-        maxIndex = rightChildIndex;
+    // if child element with this index exists
+    // find faster thread index
+    // if there is no single faster thread
+    // check threads priority
+    if (leftChild && leftChild.endingTick < currElement.endingTick) {
+        if (rightChild && rightChild.endingTick < leftChild.endingTick) {
+            nextIndex = rightChildIndex;
+        } else if (rightChild && rightChild.endingTick === leftChild.endingTick) {
+            if (leftChild.number < rightChild.number) {
+                nextIndex = leftChildIndex;
+            } else {
+                nextIndex = rightChildIndex;
+            }
+        }else {
+            nextIndex = leftChildIndex;
+        }
     }
 
-    if (index !== maxIndex) {
-        var tmp = queue[index];
-        queue[index] = queue[maxIndex];
-        queue[maxIndex] = tmp;
-
-        this._siftDownBusy(maxIndex);
+    if (index !== nextIndex) {
+        var tmp = this.threads.busy[index];
+        this.threads.busy[index] = this.threads.busy[nextIndex];
+        this.threads.busy[nextIndex] = tmp;
+        this._siftDownBusy(nextIndex);
     }
-}
+};
 
 JobQueue.prototype.incrementTimer = function (inc) {
     this.tick += inc;
-}
+};
 
 JobQueue.prototype.isJobCompleted = function () {
     if (this.threads.busy.length) {
-        return this.tick === this.threads.busy[0].currentJob.endingTick;
+        return this.tick === this.threads.busy[0].endingTick;
     }
-}
+};
 
 JobQueue.prototype.isBusy = function () {
     return this.threads.busy.length;
-}
+};
 
 JobQueue.prototype.isThreadAvailable = function () {
     return this.threads.free.length;
-}
+};
 
 JobQueue.prototype.waitNextReady = function () {
-    while (this.tick < this.threads.busy[0].currentJob.endingTick) {
+    while (this.tick < this.threads.busy[0].endingTick) {
         this.incrementTimer(1);
     }
-}
+};
 
 JobQueue.prototype.printLogs = function () {
     this.logs.forEach(function (entry) {
         console.log(entry.thread + ' ' + entry.startTime);
     });
-}
+};
 
 JobQueue.prototype.startWork = function () {
     var jobs = this.jobs;
@@ -196,7 +211,7 @@ JobQueue.prototype.startWork = function () {
             }
         }
 
-        while (queue.isThreadAvailable()) {
+        while (queue.isThreadAvailable() && jobs[i]) {
             for (var j = i; j < jobs.length; j++) {
                 if (jobs[j] && queue.isThreadAvailable()) {
                     queue.assignJob(j);
@@ -209,7 +224,7 @@ JobQueue.prototype.startWork = function () {
 
         queue.incrementTimer(1);
     }
-}
+};
 
 // TESTING ########################################################################################
 
@@ -228,9 +243,7 @@ rl.on('line', function(line){
 
     if (lineNum === 0) {
         threads = parseInt(line.split(' ')[0]);
-    }
-
-    if (lineNum === 1) {
+    } else {
         jobs = line.split(' ').map(function (job) {
             return parseInt(job);
         });
@@ -242,4 +255,4 @@ rl.on('line', function(line){
         process.exit();
     }
     lineNum++;
-})
+});
